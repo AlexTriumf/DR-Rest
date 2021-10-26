a2c40b98aee74b20e940534979d777c7e499d23f
AlexTriumf <TriumfAlex@gmail.com>
2021-10-26 11:21:47 +0200

    vitjekker

--- DR-Rest/Managers/ModelContext.cs
+++ DR-Rest/Managers/ModelContext.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModelLib;
 
 namespace DR_Rest.Managers
 {
-    public class ModelContext
+    public class ModelContext : DbContext
     {
+        public ModelContext(DbContextOptions<ModelContext> options) :
+            base(options)
+        {
+            //
+        }
+        public DbSet<Model> Records { get; set; }
     }
+
 }
+
