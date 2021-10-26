97168da93fb563ad1e5399f7bdb09d83b77278cf
AlexTriumf <TriumfAlex@gmail.com>
2021-10-26 10:07:51 +0200

    Model + manager

---
+++ DR-Rest/Manager/IManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model.Model;
+
+namespace DR_Rest.Manager
+{
+    interface IManager
+    {
+        IEnumerable<Model.Model>
+    }
+}
---
+++ Model/Model.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Model
+{
+    public class Model
+    {
+
+        private string _artist;
+        private string _title;
+        private int _duration;
+        private int _publicYear;
+
+        public Model()
+        {}
+
+        public Model(string artist, string title, int duration, int publicYear)
+        {
+            _artist = artist;
+            _title = title;
+            _duration = duration;
+            _publicYear = publicYear;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_artist)}: {_artist}, {nameof(_title)}: {_title}, {nameof(_duration)}: {_duration}, {nameof(_publicYear)}: {_publicYear}";
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public int Duration
+        {
+            get => _duration;
+            set => _duration = value;
+        }
+
+        public int PublicYear
+        {
+            get => _publicYear;
+            set => _publicYear = value;
+        }
+    }
+}
